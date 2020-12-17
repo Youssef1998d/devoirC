006ac3149b1808b31d59e7965f66a2c6b74b5140
Youssef1998d <50227410+Youssef1998d@users.noreply.github.com>
2020-12-17 18:10:00 +0100

    Update bib.h

--- bib.h
+++ bib.h
@@ -5,14 +5,14 @@ typedef struct MaDate{
     int jj;
     int mm;
     int aa;
-}MaDate;
+}MaDate;                //La definition de la structure MaDate (Question n°1)
 
 typedef struct Etudiant{
     char cn[8];
     char nom[20];
     char prenom[20];
     MaDate dateNais;
-}Etudiant;
+}Etudiant;              //La definition de la structure Etudiant (Question n°1)
 
 int SetPos(){
     int x=-1;
@@ -20,7 +20,7 @@ int SetPos(){
         scanf("%d", &x);
     }
     return x;
-}
+}                       //Question n°2)a)
 
 int Set4(){
     int x=0;
@@ -28,12 +28,12 @@ int Set4(){
         scanf("%d", &x);
     }
     return x;
-}
+}                       //Question n°2)b)
 
 void setDate(MaDate *d){
     MaDate newDate={SetPos(), SetPos(), Set4()};
     *d = newDate;
-}
+}                       //Question n°2)c)
 
 void setEtudiant(Etudiant *E){
     char n[20], p[20], c[8];
@@ -48,13 +48,13 @@ void setEtudiant(Etudiant *E){
     scanf("%s", newE.cn);
     newE.dateNais = date;
     *E = newE;
-}
-void affDate(MaDate d){
+}                       //Question n°2)d)
+void infDate(MaDate d){
     printf("Date de naissance : %d/%d/%d", d.jj, d.mm, d.aa);
-}
-void affEt(Etudiant e){
+}                       //Question n°2)e)
+void infEt(Etudiant e){
     printf("Nom : %s ", e.nom);
     printf("Prenom : %s \nCIN : %s \n", e.nom, e.prenom, e.cn);
     affDate(e.dateNais);
-}
+}                       //Question n°2)f)
 #endif // BIB_H_INCLUDED
