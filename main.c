fa4d5a433b30ae5223630fccc65a33af17ee0970
Youssef1998d <50227410+Youssef1998d@users.noreply.github.com>
2020-12-17 18:03:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,19 @@
 #include "bib.h"
 
 int main()
-{
-    Etudiant p1;
-    setEtudiant(&p1);
-    affEt(p1);
+{   int nbr;
+
+    printf("Entrer le nbr des etudiants : ");
+    scanf("%d", &nbr);              //Pour lire le nombre des etudiants
+
+    Etudiant tabEtu[nbr];           //La table ou va enregistrer les etudiants
+    for (int i=0; i<nbr;i++){
+        setEtudiant(&tabEtu[i]);
+    }                               //Enregistrements des etudiants est faite
+    for (int i=0;i<nbr;i++){
+        if ((tabEtu[i].dateNais.jj+tabEtu[i].dateNais.mm*100+tabEtu[i].dateNais.aa*10000)>=19900101){
+            affEt(tabEtu[i]);
+        }
+    }                               //affichage des etudiants dont leurs date de naissance > 01/01/1990
     return 0;
 }
